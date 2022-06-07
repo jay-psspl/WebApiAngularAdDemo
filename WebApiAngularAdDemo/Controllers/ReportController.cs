432c7ad19ec32b502a6774a9196b9aa48ce4d7fa
jay-psspl <jay@prakashinfotech.com>
2022-06-07 18:53:26 +0530

    Add project files.

---
+++ WebApiAngularAdDemo/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAngularAdDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:scopes")]
+    public class ReportController : ControllerBase
+    {
+        [Authorize(Roles = "Manager")]
+        [HttpGet("[action]")]
+        public IActionResult GetReport()
+        {
+            return File(System.IO.File.ReadAllBytes(@"C:\Users\jay\Downloads\Preparing for AZ-900 Article_Skylines Academy.pdf"), "application/pdf");
+        }
+
+        /*[Authorize]
+        [HttpGet("[action]")]
+        public IActionResult GetReportStatus()
+        {
+            return Ok(new { Status = @"Report Generated at - " + DateTime.Now.ToString("MM-dd-yyyy HH:mm:ss") });
+        }*/
+    }
+}
